5d0eaa3f70c20830e8c869f2971066fd3f4eeeb3
Amrit <76801862+retrogtx@users.noreply.github.com>
2023-01-22 13:12:57 +0530

    Update generate.py

--- generate.py
+++ generate.py
@@ -3,5 +3,11 @@ import random
 # coin = random.choice(["heads", "tails"])
 # print(coin)
 
-number = random.randint(1, 10)
-print(number)
+# number = random.randint(1, 10)
+# print(number)
+
+cards = ["jack", "queen", "king"]
+random.shuffle(cards)
+
+for card in cards:
+    print(card)
