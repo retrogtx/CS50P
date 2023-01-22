9761bda4a8ab97b09df896ac5074f84a6bf612ea
Amrit <76801862+retrogtx@users.noreply.github.com>
2023-01-22 18:55:12 +0530

    Update name.py

--- name.py
+++ name.py
@@ -5,9 +5,13 @@ import sys
 # except IndexError:
 #     print("Too few arguments.")
 
+# Check for errors
 if len(sys.argv) < 2:
     print("Too few arguments.")
+
 elif len(sys.argv) > 2:
     print("Too many arguments.")
+
+# Print if everthing is right
 else:
     print("Hello, my name is", sys.argv[1])
