1c6ddfe1d1aa7e3229b032874198747a196b54c8
Amrit <76801862+retrogtx@users.noreply.github.com>
2023-03-19 21:12:53 +0530

    Went through week 2

--- cat.py
+++ cat.py
@@ -1,4 +1,40 @@
-i = 3
-while i != 0:
-    print("meow")
-    i = i - 1
+# i = 0
+# while i < 3:
+#     print("meow")
+#     i += 1
+
+# for i in [0, 1, 2]:
+#     print("meow")
+
+# for _ in range(3):  # _ is basically a temporary variable you don't need anymore
+#     print("meow")
+
+
+# print("meow\n" * 3, end="")
+
+# while True:
+#     n = int(input("What is n? "))
+#     if n > 0:
+#         break
+
+# for _ in range(n):
+#     print("meow")
+
+def main():
+    number = get_number()
+    meow(number)
+
+
+def get_number():
+    while True:
+        n = int(input("What is n? "))
+        if n > 0:
+            return n
+
+
+def meow(n):
+    for _ in range(n):
+        print("meow")
+
+
+main()
---
+++ hogwarts.py
@@ -0,0 +1,18 @@
+# students = ["Hermoine", "Harry", "Ron", "Draco"]
+
+# for i in range(len(students)):
+#     print(i + 1, students[i])
+
+# houses = ["Gryffindor", "Gryffindor", "Gryffindor", "Slytherin"]
+
+students = {
+    "Hermione": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Ron": "Gryffindor",
+    "Draco": "Slytherin"
+}
+
+for student in students:
+    print(student, students[student], sep=": ")
+
+# stopped at 56:29 at week 2 loops
