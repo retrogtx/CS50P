60b8b06691c4ac92e6ac3ef8e091f6ffbb75bce1
Amrit <76801862+retrogtx@users.noreply.github.com>
2023-03-19 19:46:17 +0530

    New lessons from Week 1 and 2

---
+++ cars.py
@@ -0,0 +1,7 @@
+cars = ['audi', 'mercedes', 'ferrari', 'jaguar', 'tesla']
+cars.sort()
+print(cars)
+
+cars.sort(reverse=True)
+print(cars)
+print(len(cars))
---
+++ compare.py
@@ -0,0 +1,16 @@
+x = int(input("What is x? "))
+y = int(input("What is y? "))
+
+# if x < y:
+#     print("X is less than Y.")
+# if x > y:
+#     print("X is greater than Y.")
+
+# if x == y:
+#     print("They are both equal.")
+
+if x > y or x < y:
+    print("They are both not equal.")
+
+else:
+    print("They are both equal.")
