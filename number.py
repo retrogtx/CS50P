89cc1850f13f3d17fbb0bafcd5d183592351c0dc
Amrit <76801862+retrogtx@users.noreply.github.com>
2023-03-24 18:06:25 +0530

    Week 3 - Exceptions

---
+++ number.py
@@ -0,0 +1,14 @@
+def main():
+    x = get_int("What is x? ")
+    print(f"x is {x}")
+
+
+def get_int(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            pass
+
+
+main()
