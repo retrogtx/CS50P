a345c5d8e2333f6af3198991a7c40ddb9d57bc1a
Amrit <76801862+retrogtx@users.noreply.github.com>
2023-03-26 12:42:19 +0530

    Week 4 - Libraries
    done and dusted.

--- name.py
+++ name.py
@@ -1,24 +1,7 @@
 import sys
 
-# try:
-#     print("Hello, my name is", sys.argv[1])
-# except IndexError:
-#     print("Too few arguments.")
-
-# Check for errors
-# if len(sys.argv) < 2:
-#     sys.exit("Too few arguments.")
-
-# elif len(sys.argv) > 2:
-#     sys.exit("Too many arguments.")
-
-# # Print if everthing is right
-# print("Hello, my name is", sys.argv[1])
-
 if len(sys.argv) < 2:
     sys.exit("Too few arguments")
 
 for arg in sys.argv[1:]:
     print("Hello, my name is", arg)
-
-# week 4 libraries 27.14
