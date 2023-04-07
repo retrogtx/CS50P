3c6a9eeb6169ea689192c4f01dd01112e3651793
Amrit <76801862+retrogtx@users.noreply.github.com>
2023-04-07 19:17:54 +0530

    Update validate.py

--- validate.py
+++ validate.py
@@ -1,9 +1,8 @@
+import re
+
 email = input("What is your email? ").strip()
 
-username, domain = email.split("@")
-
-if username and domain.endwith(".edu"):
+if re.search(".+@.+", email):
     print("Valid")
-
 else:
     print("Invalid")
