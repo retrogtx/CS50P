c9cf89812034ab99c17c22f8449d799b263366ce
Amrit <76801862+retrogtx@users.noreply.github.com>
2023-04-07 18:00:58 +0530

    Week 6 - File I/O

--- students.py
+++ students.py
@@ -1,3 +1,11 @@
+students = []
+
 with open("students.csv") as file:
     for line in file:
-        row = line.rstrip().split(",")
+        name, house = line.rstrip().split(",")
+        student = {"name": name, "house": house}
+        students.append(student)
+
+
+for student in sorted(students, key=lambda student: student["name"]):
+    print(f"{student['name']} is in {student['house']}")
