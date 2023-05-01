e57823ace4c143c09503ef2aee62ec2e42beb26d
Amrit Rai <iamamrit27@gmail.com>
2023-05-01 18:21:34 +0530

    Update student.py

--- student.py
+++ student.py
@@ -1,23 +1,25 @@
 class Student:
     def __init__(self, name, house):
+        if not name:
+            raise ValueError("Missing name")
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
         self.name = name
         self.house = house
 
 
 def main():
     student = get_student()
-    print(f"{student.name} from {student.house}")  # type: ignore
+    print(f"{student.name} from {student.house}")
 
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    student = Student(name, house)  # type: ignore
-    return student
+    return Student(name, house)
 
 
 if __name__ == "__main__":
     main()
 
-
-# Week 8 - 40
+# Week 8 - 1:00:00
