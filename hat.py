eb2822b414420ea37d4f95049bbdeac9396d446b
Amrit Rai <iamamrit27@gmail.com>
2023-05-02 17:45:28 +0530

    Create hat.py

---
+++ hat.py
@@ -0,0 +1,16 @@
+import random
+
+
+class Hat:
+    def __init__(self):
+    self.houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+
+    def sort(self, name):
+        print(name, "is in", random.choice(self.houses))
+
+
+hat = Hat()
+hat.sort("Harry")
+
+
+# Week 8 - 2:00:00
