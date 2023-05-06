ee99f0bc176d302c78bf2df5bf600962cfa2f7ee
Amrit Rai <iamamrit27@gmail.com>
2023-05-06 10:40:54 +0530

    Takes argument from terminal now.

--- meows.py
+++ meows.py
@@ -1,7 +1,10 @@
-def meow(n: int) -> str:
-    return "meow\n" * n
+import sys
 
-
-number: int = int(input("Number: "))
-meows: str = meow(number)
-print(meows, end="")
+if len(sys.argv) == 1:
+    print("meow")
+elif len(sys.argv) == 3 and sys.argv[1] == "-n":
+    n = int(sys.argv[2])
+    for _ in range(n):
+        print("meow")
+else:
+    print("usage: meows.py")
